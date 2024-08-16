1d5c25fd5bd6e38b1022035585acb739d4609383
Adrian Rueda <drilan@gmail.com>
2024-08-16 23:27:37 +0200

    Mejorado el estilo

--- src/character-sheet-component.tsx
+++ src/character-sheet-component.tsx
@@ -293,18 +293,18 @@ const CharacterSheet: React.FC = () => {
 
   return (
     <div className="max-w-4xl mx-auto p-4 bg-gray-100 rounded-lg shadow">
-      <div className="flex justify-between mb-4">
+      <div className="flex flex-col sm:flex-row justify-between mb-4">
         <input
-          className="p-2 border rounded"
+          className="p-2 border rounded mb-2 sm:mb-0 sm:mr-2 w-full sm:w-auto"
           placeholder="PLAYER NAME"
           value={character.name}
           onChange={(e) =>
             handleInputChange("name", "name", e.target.value)
           }
         />
-        <div className="flex items-center space-x-2">
+        <div className="flex items-center space-x-2 w-full sm:w-auto">
           <select
-            className="p-2 border rounded"
+            className="p-2 border rounded w-full sm:w-auto"
             value={character.class}
             onChange={handleClassChange}
           >
