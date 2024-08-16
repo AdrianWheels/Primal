b176c7e834f4fba31ac17645f2a01d095840f2ae
Adrian Rueda <drilan@gmail.com>
2024-08-17 01:23:12 +0200

    Añadido logo y menu sticky

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,38 @@ import CharacterSheet from "./character-sheet-component";
 import MonsterLifeTracker from "./monster-life-tracker";
 import CampaignTracker from "./CampaignTracker"; // Importa el nuevo componente
 import "./style/tailwind.css";
+import logo from "./iconos/logo blanco.png"; // Asegúrate de que la ruta sea correcta
 
 const App = () => {
   return (
     <Router>
       <div className="min-h-screen flex flex-col">
-        <nav className="bg-black text-white p-4 shadow-md">
-          <ul className="flex space-x-4">
-            <li>
-              <Link to="/" className="hover:text-yellow-500">Home</Link>
-            </li>
-            <li>
-              <Link to="/campaign-tracker" className="hover:text-yellow-500">Campaign Tracker</Link>
-            </li>
-            <li>
-              <Link to="/character-sheet" className="hover:text-yellow-500">Character Sheet</Link>
-            </li>
-            <li>
-              <Link to="/monster-life-tracker" className="hover:text-yellow-500">Monster Life Tracker</Link>
-            </li>
-            
-          </ul>
+        {/* Logo visible arriba */}
+        <div className="bg-black p-4 flex justify-center md:hidden">
+          <img src={logo} alt="Logo" className="h-12" />
+        </div>
+        
+        {/* Barra de navegación sticky */}
+        <nav className="bg-black text-white p-4 shadow-md sticky top-0 z-40">
+          <div className="relative flex items-center justify-between">
+            <ul className="flex space-x-4 md:space-x-8">
+              <li>
+                <Link to="/" className="hover:text-yellow-500">Home</Link>
+              </li>
+              <li>
+                <Link to="/campaign-tracker" className="hover:text-yellow-500">Campaign Tracker</Link>
+              </li>
+              <li>
+                <Link to="/character-sheet" className="hover:text-yellow-500">Character Sheet</Link>
+              </li>
+              <li>
+                <Link to="/monster-life-tracker" className="hover:text-yellow-500">Monster Life Tracker</Link>
+              </li>
+            </ul>
+            <div className="hidden md:flex justify-center w-full absolute left-1/2 transform -translate-x-1/2">
+              <img src={logo} alt="Logo" className="h-12" />
+            </div>
+          </div>
         </nav>
 
         <main className="flex-grow p-4">
